360afc9f4cb23a2d77aca9bbadb1e27e4a1cf7de
Natālija Vernere <natalija.vernere@tietoevry.com>
2023-06-13 12:06:25 +0300

    Delete deprecated files

--- DataAccess/DbAccess/ISqlDataAccess.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DataAccessProject.DbAccess;
-
-public interface ISqlDataAccess
-{
-    Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters);
-    Task SaveData<T>(string storedProcedure, T parameters);
-}
